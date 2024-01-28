a112790b27f4e2cc97a734294fa4f6118251ad12
0703kyj <127181370+0703kyj@users.noreply.github.com>
2024-01-28 17:18:38 +0900

    feat: 프로젝트 기본 설정 (#1)
    * feat: 스웨거 API DOCS 설정 추가
    
    * feat: spring-data-jpa, mysql 의존성 추가
    
    * feat: spring-data-jpa, mysql 기본 설정
    
    application.yml 파일 수정함
    
    * fix: Test에서 h2 데이터베이스 메모리 모드로 사용
    
    test에서 사용하는 데이터베이스를 간단하게 메모리에서 동작하도록 h2로 변경했고 그에 따른 의존성 추가하였습니다.

---
+++ subprojects/api/src/main/java/com/poemfoot/api/domain/hello/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.poemfoot.api.domain.hello.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Tag(name = "Hello", description = "테스트")
+@RequestMapping("/hello")
+public class HelloController {
+
+    @Operation(summary = "Swagger 테스트")
+    @GetMapping
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("hello");
+    }
+}
