4829b3e1bac73279403f55731fdc45452303d9cf
0703kyj <kyj0703@konkuk.ac.kr>
2024-01-29 13:36:30 +0900

    feature: 테스트용 member domain 추가

---
+++ subprojects/api/src/main/java/com/poemfoot/api/domain/member/controller/MemberController.java
@@ -0,0 +1,29 @@
+package com.poemfoot.api.domain.member.controller;
+
+import com.poemfoot.api.domain.member.dao.MemberRepository;
+import com.poemfoot.api.domain.member.domain.Member;
+import jakarta.annotation.PostConstruct;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberController {
+    private final MemberRepository memberRepository;
+
+    @GetMapping
+    public List<Member> findAll() {
+        return memberRepository.findAll();
+    }
+
+    @PostConstruct
+    public void init() {
+        memberRepository.save(new Member("poemfoot1"));
+        memberRepository.save(new Member("poemfoot2"));
+        memberRepository.save(new Member("poemfoot3"));
+    }
+}
