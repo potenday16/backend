13bb995f0e906905b6c1fe6ec84129a38162a23e
0703kyj <kyj0703@konkuk.ac.kr>
2024-02-01 19:26:04 +0900

    test: Member deviceOS 관련 테스트 추가

--- subprojects/api/src/test/java/com/poemfoot/api/service/MemberServiceTest.java
+++ subprojects/api/src/test/java/com/poemfoot/api/service/MemberServiceTest.java
@@ -1,7 +1,6 @@
 package com.poemfoot.api.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import com.poemfoot.api.domain.member.DeviceOsType;
 import com.poemfoot.api.domain.member.Member;
@@ -10,8 +9,6 @@ import com.poemfoot.api.dto.response.member.MemberResponse;
 import com.poemfoot.api.exception.notfound.member.NotFoundMemberException;
 import com.poemfoot.api.repository.MemberRepository;
 import java.util.List;
-import java.util.Optional;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -30,19 +27,21 @@ class MemberServiceTest {
 
     String deviceId;
     String nickname;
-    String deviceOs;
+    String deviceIOS;
+    String deviceAndroid;
 
     @BeforeEach
     void beforeEach() {
         deviceId = "HDFA3dfd23";
         nickname = "test";
-        deviceOs = "iOS";
+        deviceIOS = "iOS";
+        deviceAndroid = "Android";
     }
 
     @Test
     @DisplayName("사용자를 저장한다.")
     void saveMemberTest() {
-        MemberRequest memberRequest = new MemberRequest(nickname, deviceOs);
+        MemberRequest memberRequest = new MemberRequest(nickname, deviceIOS);
         MemberResponse memberResponse = memberService.saveMember(deviceId, memberRequest);
 
         Member member = memberRepository.findById(memberResponse.id())
@@ -56,9 +55,27 @@ class MemberServiceTest {
     }
 
     @Test
+    @DisplayName("사용자 기기 os를 정확하게 등록한다.")
+    void saveMemberDeviceOsTest() {
+        String androidDeviceId = "test";
+        MemberRequest IOSRequest = new MemberRequest(nickname, deviceIOS);
+        MemberRequest AndroidRequest = new MemberRequest(nickname, deviceAndroid);
+        MemberResponse IOSResponse = memberService.saveMember(deviceId, IOSRequest);
+        MemberResponse AndroidResponse = memberService.saveMember(androidDeviceId, AndroidRequest);
+
+        Member iOSMember = memberRepository.findById(IOSResponse.id())
+                .orElseThrow(NotFoundMemberException::new);
+        Member androidMember = memberRepository.findById(AndroidResponse.id())
+                .orElseThrow(NotFoundMemberException::new);
+
+        assertThat(iOSMember.getDeviceOs()).isEqualTo(DeviceOsType.IOS);
+        assertThat(androidMember.getDeviceOs()).isEqualTo(DeviceOsType.ANDROID);
+    }
+
+    @Test
     @DisplayName("이미 등록된 사용자인 경우 true 를 반환한다.")
     void checkSavedMemberTest() {
-        MemberRequest memberRequest = new MemberRequest(nickname, deviceOs);
+        MemberRequest memberRequest = new MemberRequest(nickname, deviceIOS);
         MemberResponse memberResponse = memberService.saveMember(deviceId, memberRequest);
 
         assertThat(memberService.findSavedMember(deviceId).isRegistered()).isTrue();
