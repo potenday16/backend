ec260130220b00517a87082336aa295f9149cc11
0703kyj <kyj0703@konkuk.ac.kr>
2024-02-02 14:43:35 +0900

    fix: AttributeConverter관련 오류 수정

--- subprojects/api/src/main/java/com/poemfoot/api/converter/WordsConverter.java
+++ subprojects/api/src/main/java/com/poemfoot/api/converter/WordsConverter.java
@@ -1,6 +1,8 @@
 package com.poemfoot.api.converter;
 
 import com.poemfoot.api.domain.Words;
+import com.poemfoot.api.exception.server.poem.FailStringToWordsException;
+import com.poemfoot.api.exception.server.poem.FailWordsToStringException;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 import java.util.Arrays;
@@ -12,7 +14,7 @@ public class WordsConverter implements AttributeConverter<Words, String> {
     @Override
     public String convertToDatabaseColumn(Words attribute) {
         if (attribute == null) {
-            return null;
+            throw new FailWordsToStringException();
         }
         return String.join(WORDS_DELIMITER, attribute.words());
     }
@@ -20,7 +22,7 @@ public class WordsConverter implements AttributeConverter<Words, String> {
     @Override
     public Words convertToEntityAttribute(String dbData) {
         if (dbData == null) {
-            return null;
+            throw new FailStringToWordsException();
         }
         return Words.of(Arrays.stream(dbData.split(WORDS_DELIMITER)).toList());
     }
