6316d114e2eabae0be92493b4a1691473549170d
0703kyj <127181370+0703kyj@users.noreply.github.com>
2024-02-03 19:48:55 +0900

    feat: CardPoemResponse 시 생성 시간 추가 (#18)
    * feat: CardPoemResponse 시 생성 시간 추가
    
    * feat: CardPoemResponse 주소명 추가

--- subprojects/api/src/main/java/com/poemfoot/api/dto/request/CardPoemRequest.java
+++ subprojects/api/src/main/java/com/poemfoot/api/dto/request/CardPoemRequest.java
@@ -2,6 +2,7 @@ package com.poemfoot.api.dto.request;
 
 public record CardPoemRequest(
         String location,
+        String address,
         double latitude,
         double longitude
 ) {}
--- subprojects/api/src/main/java/com/poemfoot/api/dto/response/card/CardPoemResponse.java
+++ subprojects/api/src/main/java/com/poemfoot/api/dto/response/card/CardPoemResponse.java
@@ -3,21 +3,25 @@ package com.poemfoot.api.dto.response.card;
 import com.poemfoot.api.domain.W3wResult;
 import com.poemfoot.api.domain.Words;
 import com.poemfoot.api.domain.poem.Poem;
+import java.sql.Timestamp;
 import lombok.Builder;
 
 @Builder
 public record CardPoemResponse(
         Long poemId,
         Long w3wResultId,
+        Timestamp createdTime,
+        String address,
         String title,
         String content,
         Words words,
-        String location
-) {
-    public static CardPoemResponse of(Poem poem, W3wResult w3wResult) {
+        String location) {
+    public static CardPoemResponse of(Poem poem, W3wResult w3wResult, String address) {
         return CardPoemResponse.builder()
                 .poemId(poem.getId())
                 .w3wResultId(w3wResult.getId())
+                .createdTime(poem.getCreatedTime())
+                .address(address)
                 .title(poem.getTitle())
                 .content(poem.getContent())
                 .words(poem.getWords())
