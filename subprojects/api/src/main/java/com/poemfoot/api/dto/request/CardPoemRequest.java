15d70d8de40d8ab7b53c61d9d02a1d3a51c8507a
0703kyj <127181370+0703kyj@users.noreply.github.com>
2024-02-14 21:40:47 +0900

    [POEM-7] fix: 시 생성 시, 빈 문자열 예외처리 (#20)
    * chore: spring validation 추가
    
    * fix: [POEM-7] 시 생성 시, 빈 문자열 예외처리
    
    * chore: spring validation 의존성 줄바꿈
    
    13번 줄 -> 4번 줄

--- subprojects/api/src/main/java/com/poemfoot/api/dto/request/CardPoemRequest.java
+++ subprojects/api/src/main/java/com/poemfoot/api/dto/request/CardPoemRequest.java
@@ -1,7 +1,11 @@
 package com.poemfoot.api.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record CardPoemRequest(
+        @NotBlank(message = "위치 단어는 공백일 수 없습니다.")
         String location,
+        @NotBlank(message = "현재 위치는 공백일 수 없습니다.")
         String address,
         double latitude,
         double longitude
